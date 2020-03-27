b2f711c03e59774949e7cc4bf435f5757e19652d
Matt Howlett <matt.howlett@gmail.com>
2020-03-27 16:14:48 -0700

    trogdor_runner.py

--- tests/trogdor/trogdor_runner.py
+++ tests/trogdor/trogdor_runner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2019 Confluent Inc.
+# Copyright 2019-2020 Confluent Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,16 +13,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 
 import argparse
 import json
 import sys
-from produce_spec_runner import execute_produce_spec
-from trogdor_utils import update_trogdor_error
-from trogdor_utils import trogdor_log
+from .produce_spec_runner import execute_produce_spec
+from .trogdor_utils import update_trogdor_error
+from .trogdor_utils import trogdor_log
 
-parser = argparse.ArgumentParser(description='Python Trogdor Producer.')
+parser = argparse.ArgumentParser(description='Python Trogdor Producer')
 parser.add_argument('--spec', dest='spec', required=False)
 args = parser.parse_args()
 
@@ -30,37 +30,31 @@ args = parser.parse_args()
 def execute_task(workload):
     try:
         spec_class = workload["class"]
-        if spec_class == 'org.apache.kafka.trogdor.workload.ProduceBenchSpec':
+        if spec_class == "org.apache.kafka.trogdor.workload.ProduceBenchSpec":
             execute_produce_spec(workload)
         else:
-            update_trogdor_error(spec_class + " is not supported.")
+            update_trogdor_error("Tasks of type " + spec_class + " not supported")
     except Exception as exp:
         update_trogdor_error("Exception:{}".format(exp))
 
 
-def get_task_from_specfile(specFile):
+def get_task_from_specfile(spec_file_path):
     """
-    :param specFile: One Trogdor ExternalCommandSpec JSON file
-    :return specFile["spec"]["workload"] or None
+    :param spec_file_name: Trogdor ExternalCommandSpec JSON file path
+    :return: spec["workload"]
     """
-    specString = ""
-    workload = None
-    with open(specFile) as f:
-        for line in f:
-            if line.startswith('#'):
-                continue
-            specString += line
-    if (specString == ""):
-        return None
-    task = json.loads(specString)
+    spec_string = ""
+    with open(spec_file_path) as f:
+        spec_string = f.read()
+    task = json.loads(spec_string)
     if task.get("class", "") == "org.apache.kafka.trogdor.workload.ExternalCommandSpec" and "workload" in task:
-        workload = task["workload"]
-    return workload
+        return task["workload"]
+    raise TypeError("Invalid ExternalCommandSpec")
 
 
 def get_task_from_input():
     """
-    Waiting for a new task on stdin, formatted as {"id":<task ID string>, "workload":<configured workload JSON object>}
+    Wait for a new task on stdin, formatted as {"id":<task ID string>, "workload":<configured workload JSON object>}
     :return: spec["workload"]
     """
     trogdor_log("Python Trogdor Runner is Ready.")
@@ -70,7 +64,7 @@ def get_task_from_input():
             trogdor_log("Input line " + line)
             comm = json.loads(line)
             if "workload" in comm and "id" in comm:
-                trogdor_log("Received one workload:{}".format(comm))
+                trogdor_log("Received workload: {}".format(comm))
                 return comm["workload"]
         except ValueError:
             trogdor_log("Input line " + line + " is not a valid external runner command.")
